36376671ff96698e9cdb48bfd8e98aedff7cf97c
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-11-20 01:07:16 +0530

    Refactoring UX

--- frontend/src/components/Hero/hero.js
+++ frontend/src/components/Hero/hero.js
@@ -4,28 +4,29 @@ import { Container, Row, Col } from "reactstrap";
 import BackgroundAnimation from "../BackgroundAnimation/BackgroundAnimation";
 
 const HeroComponent = ({ isLanding, size }) => (
-  <Container className="py-lg-md d-flex">
-    <div className="col px-0">
-      <Row>
-        {isLanding && (
-          <Col lg="6">
-            <h1 className="display-3 padding-top text-white">
-              Something just like this
-              <span>completed with examples</span>
-            </h1>
-            <p className="lead text-white">
-              The design system comes with four pre-built pages to help you get
-              started faster. You can change the text and images and you're good
-              to go.
-            </p>
-          </Col>
-        )}
-        <Col lg={size} className="background-animation">
-          <BackgroundAnimation />
-        </Col>
-      </Row>
-    </div>
-  </Container>
+	<Container className="py-lg-md d-flex">
+		<div className="col px-0">
+			<Row>
+				{isLanding && (
+					<Col lg="6">
+						<h1 className="display-3 padding-top text-white">
+							Loop
+							<span style={{ fontSize: "0.7em" }}>
+								Supporting creators 3.0
+							</span>
+						</h1>
+						<p className="lead text-white">
+							A platform that helps you support your favourite
+							creators without draining your pockets.
+						</p>
+					</Col>
+				)}
+				<Col lg={size} className="background-animation">
+					<BackgroundAnimation />
+				</Col>
+			</Row>
+		</div>
+	</Container>
 );
 
 export default HeroComponent;
--- frontend/src/views/IndexSections/Download.js
+++ frontend/src/views/IndexSections/Download.js
@@ -1,23 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 
-const Download = () => (
-  <>
-    <section className="section section-lg">
-      <Container>
-        <Row className="row-grid justify-content-center">
-          <Col className="text-center" lg="8">
-            <h2 className="display-3">
-              Do you love this awesome{" "}
-              <span className="text-success">
-                Design System for Bootstrap 4?
-              </span>
-            </h2>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  </>
-);
+const Download = () => <></>;
 
 export default Download;
